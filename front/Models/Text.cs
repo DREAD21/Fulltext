71351d1f675673248e73b276fe0f6f207c6bee74
DREAD21 <87900013+DREAD21@users.noreply.github.com>
2022-04-30 15:52:33 +0300

    Update Text.cs

--- front/Models/Text.cs
+++ front/Models/Text.cs
@@ -5,9 +5,7 @@ namespace front.Models
 {
     public class Text
     {
-        [RegularExpression(@"[0-9]+", ErrorMessage="Неверные данные")]
         public int Id { get; set; }
-        [RegularExpression(@"[a-bA-B]+", ErrorMessage="Неверные данные")]
         public string text { get; set; }
     }
 }
