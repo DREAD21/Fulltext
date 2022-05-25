7df3cd2f33c1940609b0d83b1613754968e0db73
DREAD21 <87900013+DREAD21@users.noreply.github.com>
2022-05-25 19:44:58 +0300

    Update HomeController.cs

--- front/Controllers/HomeController.cs
+++ front/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Diagnostics;
 using System.Linq;
 using front.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 
 
